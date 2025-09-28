bef34f5cfc47e6e8492bce4f81a90fe8a1efded4
Sureshkumar <sureshkumar.t@praniontech.com>
2025-09-28 07:16:19 +0530

    test: add test for single number feat: implement single number support

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -4,8 +4,8 @@ from string_calculator import add
 def test_empty_string_returns_zero():
     assert add("") == 0
 
-# def test_single_number_returns_value():
-#     assert add("1") == 1
+def test_single_number_returns_value():
+    assert add("1") == 1
 
 # def test_two_numbers_comma_separated():
 #     assert add("1,5") == 6
