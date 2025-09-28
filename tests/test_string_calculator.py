baf1b6c9f56decca44ed552eeb021d3e399c7b04
Sureshkumar <sureshkumar.t@praniontech.com>
2025-09-28 07:17:02 +0530

    test: add test for two comma numbers feat: implement comma-separated sum

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -7,8 +7,8 @@ def test_empty_string_returns_zero():
 def test_single_number_returns_value():
     assert add("1") == 1
 
-# def test_two_numbers_comma_separated():
-#     assert add("1,5") == 6
+def test_two_numbers_comma_separated():
+    assert add("1,5") == 6
 
 # def test_many_numbers():
 #     assert add("1,2,3,4,5") == 15
