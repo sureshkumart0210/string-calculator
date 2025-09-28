59b754194247c62fe7f1f65a2c8bd33b79ffc288
Sureshkumar <sureshkumar.t@praniontech.com>
2025-09-28 07:20:31 +0530

    test: negative numbers raise feat: raise ValueError listing all negative numbers

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -19,10 +19,10 @@ def test_newlines_between_numbers():
 def test_custom_single_char_delimiter():
     assert add("//;\n1;2") == 3
 
-# def test_negative_raises_single():
-#     with pytest.raises(ValueError) as exc:
-#         add("1,-2,3")
-#     assert "negative numbers not allowed -2" in str(exc.value)
+def test_negative_raises_single():
+    with pytest.raises(ValueError) as exc:
+        add("1,-2,3")
+    assert "negative numbers not allowed -2" in str(exc.value)
 
 # def test_negative_raises_multiple():
 #     with pytest.raises(ValueError) as exc:
