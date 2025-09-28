2e222ede6ae3e35d941bf253139727db6a626f7d
Sureshkumar <sureshkumar.t@praniontech.com>
2025-09-28 07:18:26 +0530

    test: add test for newline delimiter feat: support newline and comma delimiters

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -13,8 +13,8 @@ def test_two_numbers_comma_separated():
 def test_many_numbers():
     assert add("1,2,3,4,5") == 15
 
-# def test_newlines_between_numbers():
-#     assert add("1\n2,3") == 6
+def test_newlines_between_numbers():
+    assert add("1\n2,3") == 6
 
 # def test_custom_single_char_delimiter():
 #     assert add("//;\n1;2") == 3
