66f901e8b4846f0e25f8733b7689924e08b34e8d
Sureshkumar <sureshkumar.t@praniontech.com>
2025-09-28 07:19:54 +0530

    test: add test for custom delimiter feat: implement custom delimiter parsing

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -16,8 +16,8 @@ def test_many_numbers():
 def test_newlines_between_numbers():
     assert add("1\n2,3") == 6
 
-# def test_custom_single_char_delimiter():
-#     assert add("//;\n1;2") == 3
+def test_custom_single_char_delimiter():
+    assert add("//;\n1;2") == 3
 
 # def test_negative_raises_single():
 #     with pytest.raises(ValueError) as exc:
